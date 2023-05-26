254be0ea46b3a9b723b9f0246e5ed9b8fd4ebbf1
Delfina Laora <delphine2203.miel@gmail.com>
2023-05-26 19:48:07 +0200

    Ajout bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -17,83 +17,65 @@ const slides = [
 	}
 ]
 
-// for(let i = 0; i < slides.length; i++) {
-// 	const dotElement = document.createElement("div");
-// 	dotElement.classList.add("dot");
-// 	const dotsElement = document.querySelector(".dots");		
-// 	dotsElement.appendChild(dotElement);		
-// }
+const bannerElement = document.querySelector("#banner");	
 
-// for(let i = 0; i < slides.length; i++) {
-// 	const imageElement = document.createElement("img");
-// 	imageElement.classList.add(".banner-img");
-	const bannerElement = document.querySelector("#banner");		
-// 	bannerElement.appendChild(imageElement);		
-// }
-	
-	
-// for (i = 0; i < slides.length; i++) {
-	// let image = document.getElementsByTagName('img');
-	// img.src = slides[i].image;
-	// image.className = ("banner-img");
-	let baliseImage = document.getElementById("baliseImage")
-	let tagLine = document.getElementById('baliseTagLine');
-	// let tagLine = document.getElementsByTagName('p');
-	// bannerElement.appendChild(tagLine)
-	// baliseImage.innerHTML = tagLine
-	// baliseImage.src = slides[0].image /*modifie l'image dans le dom*/
-	
-	const images = slides.map(slides => slides.image);/*fait appel aux images uniquement dans un nouveau tableau*/
-	// const imageDelete = images.splice(0, 1);/*enlève la première image du tableau*/
-	let i = 0
+const baliseImage = document.getElementById("baliseImage");
+const tagLine = document.getElementById("baliseTagLine");
+const bannerImg = document.getElementsByClassName("banner-img")	
+const dotElement = document.getElementsByClassName("dot")
 
-	baliseImage.src = images[i]
-	let slide = images.length
+let i = 0
+let slide = slides.length;
 
-	const tagLines = slides.map(slides => slides.tagLine);/*fait appel aux textes uniquement dans un nouveau tableau*/
-	// const tagLinesDelete = tagLines.splice(0, 1);/*enlève le premier paragraphe du tableau*/
+	// On modifie la source des images
+baliseImage.src = slides[i].image; /*modifie l'image dans le dom*/
 
-	// let contenuTagLine = tagLine.innerHTML;/*récupère le contenu du tagline dans le dom*/
-	let newTagLine = `<p>${tagLines[i]}</p>`/*modifie la tagline dans le dom*/
-	tagLine.innerHTML = newTagLine
+	// On modifie la source des taglines
+let newTagLine = `<p>${slides[i].tagLine}</p>`;/*modifie la tagline dans le dom*/
+tagLine.innerHTML = slides[i].tagLine;
 
+	// on fait avancer les sliders en changeant la source
 function suivant(){
-	baliseImage.src = images[i]
-	newTagLine = `<p>${tagLines[i]}</p>`/*modifie la tagline dans le dom*/
-	tagLine.innerHTML = newTagLine
+	baliseImage.src = slides[i].image;
+	newTagLine = `<p>${slides[i].tagLine}</p>`;/*modifie la tagline dans le dom*/
+	tagLine.innerHTML = slides[i].tagLine;	
 }
 
-	// Ajout listener pour flèche de gauche
-
-const arrowLeft = document.querySelector(".arrow_left");
-
-arrowLeft.addEventListener("click", function() {
-	i--;	
+	// on fait avancer les sliders en boucle
+function ShowSlide(){
 	if( i >= slide) {
 		i = 0;
 	}
 	if( i < 0) {
 		i = slide-1;
 	}
-	suivant()
-	
-		console.log("Bonjour à gauche")	
-	});
+}
 
+	// on fait avancer les dots en fonctions des images
+function dot(){	
+	for(let i = 0; i < dotElement.length; i++) {
+		dotElement[i].classList.remove('dot_selected');/*je retire dot-select de tous les points*/
+	  }	 
+	dotElement[i].classList.add('dot_selected');/*j'ajoute dot-select sur le point cliqué*/
+	}	
+	
+	// Ajout listener pour flèche de gauche
+const arrowLeft = document.querySelector(".arrow_left");
+
+arrowLeft.addEventListener("click", function() {
+	i--;	
+	ShowSlide()
+	suivant()
+	dot()
+	});
 
 	// Ajout listener pour flèche de droite
 const arrowRight = document.querySelector(".arrow_right");
 
 arrowRight.addEventListener("click", function() {
 	i++;
-	if( i >= slide) {
-		i = 0;
-	}
-	if( i < 0) {
-		i = slide-1;
-	}
+	ShowSlide()
 	suivant()
-	
-	console.log("Bonjour à droite");
-})
+	dot()
+});
 
