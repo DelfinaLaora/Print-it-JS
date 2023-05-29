dece687b3fc28417621df0452dad92a3f4cf8024
Delfina Laora <delphine2203.miel@gmail.com>
2023-05-29 19:46:07 +0200

    modification ecriture code const function

--- assets/script.js
+++ assets/script.js
@@ -17,72 +17,71 @@ const slides = [
 	}
 ]
 
-const bannerElement = document.querySelector("#banner");	
+const baliseImage = document.querySelector(".banner-img");
+const tagLine = document.querySelector("#banner p");
+const dotElements = document.getElementsByClassName("dot");/*récupère toutes les class dot*/
+const dotsElement = document.querySelector(".dots");
 
-const baliseImage = document.getElementById("baliseImage");
-const tagLine = document.getElementById("baliseTagLine");
-const bannerImg = document.getElementsByClassName("banner-img")	
-const dotElement = document.getElementsByClassName("dot")
+let currentSlide = 0
 
-// création des dots
-for(let i = 0; i < slides.length; i++) {
-	const dotElement = document.createElement("div");
-	dotElement.classList.add("dot");
-	const dotsElement = document.querySelector(".dots");		
-	dotsElement.appendChild(dotElement);		
+	// création des dots
+for(let i = 0; i < slides.length; i++) {	
+	const div = document.createElement("div");
+	div.classList.add("dot");			
+	dotsElement.appendChild(div);		
 }
 
-let i = 0
-let slide = slides.length;
-
-	// On modifie la source des images dans le DOM
-baliseImage.src = slides[i].image;
-
-	// On modifie la source des taglines dans le DOM
-tagLine.innerHTML = slides[i].tagLine;
-
-	// on fait avancer les sliders en changeant la source
-function suivant(){
-	baliseImage.src = slides[i].image;	
-	tagLine.innerHTML = slides[i].tagLine;	
+	// on change la source de l'image et du paragraphe
+function next(){
+	baliseImage.src = slides[currentSlide].image;	
+	tagLine.innerHTML = slides[currentSlide].tagLine;	
 }
 
-	// on fait avancer les sliders en boucle
-function ShowSlide(){
-	if( i >= slide) {
-		i = 0;
+	// On appel la fonction pour afficher le 1er objet de sliders
+next()
+
+	// on fait avancer le sliders en boucle
+function showSlide(){
+	if(currentSlide >= slides.length) {
+		currentSlide = 0;
 	}
-	if( i < 0) {
-		i = slide-1;
+	if(currentSlide < 0) {
+		currentSlide = slides.length - 1;
 	}
 }
-
 
 	// on fait avancer les dots en fonctions des images
 function dot(){	
-	for(let i = 0; i < dotElement.length; i++) {
-		dotElement[i].classList.remove('dot_selected');/*je retire dot-selected de tous les points*/
-	}	 
-	dotElement[i].classList.add('dot_selected');/*j'ajoute dot-selected sur le point selectionné*/
+	for(i = 0; i < dotElements.length; i++) {
+		dotElements[i].classList.remove('dot_selected');/*je retire dot-selected de tous les points*/
+	}	
+	dotElements[currentSlide].classList.add('dot_selected');/*j'ajoute dot-selected sur le point selectionné*/
 }	
+// function dot(){		/*seulement si les "dot" sont crées directement dans le DOM avec ajout de la balise "dot_selected"*/
+// 	document.querySelector('.dot_selected').classList.remove('dot_selected');/*je retire dot-selected du dernier point slectionné*/		 
+// 	dotElements[currentSlide].classList.add('dot_selected');/*j'ajoute dot-selected sur le point selectionné*/
+// }	
+
+	// Fonction qui regroupe les fonctions du slider
+function updateSlide(){
+	showSlide()
+	next()
+	dot()
+}
 	
 	// Ajout listener pour flèche de gauche
 const arrowLeft = document.querySelector(".arrow_left");
 
 arrowLeft.addEventListener("click", function() {
-	i--;	
-	ShowSlide()
-	suivant()
-	dot()
+	currentSlide--;	
+	updateSlide()
 });
 
 	// Ajout listener pour flèche de droite
 const arrowRight = document.querySelector(".arrow_right");
 
 arrowRight.addEventListener("click", function() {
-	i++;
-	ShowSlide()
-	suivant()
-	dot()
+	currentSlide++;
+	updateSlide()
 });
 
